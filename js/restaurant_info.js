64277913091c3ccadb0dd5d64c728e75611a4c60
Lian Thompson <lianythompson@gmail.com>
2019-05-10 19:55:40 -0700

    add mapbox key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoibGlhbnRob21wc29uIiwiYSI6ImNqdml2czFqNjA3MTYzeW1sbWFuZWN0dmsifQ.kry7kl5e-JawBZXGS8g8Nw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
